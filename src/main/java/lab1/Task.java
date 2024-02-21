99d738d16b7197f5648d6a43573184aea9f1a56a
Vlad <martynovq03@gmail.com>
2024-02-21 18:24:45 +0300

    fix task1

--- src/main/java/lab1/Task.java
+++ src/main/java/lab1/Task.java
@@ -1,9 +1,13 @@
 package lab1;
 
+import java.util.Arrays;
+
 public class Task {
     public static void main(String[] args) {
         String[] strings = {"abc", "aaaaaaaaaaaaa", "abcd", "aabbccddeeffgg", "abcdefrtyuikolp"};
+        Arrays.stream(strings).forEach(System.out::println);
         Consumer<String> uniqueStringFinder = new UniqueStringFinder();
+        System.out.println("\nUnique String:");
         uniqueStringFinder.accept(strings);
     }
 }
